bf1ffa11fed59ddc900931867f5c40e393252cc6
Christian Salon <christian.salon@whitecloak.com>
2024-09-09 08:29:31 +0800

    Move http client configuration to config

--- lib/modules/person/data/di/person_service_locator.dart
+++ lib/modules/person/data/di/person_service_locator.dart
@@ -1,4 +1,4 @@
-import 'package:exam/common/service/http_client.dart';
+import 'package:exam/config/http_client.dart';
 import 'package:exam/modules/person/data/data_source/remote/person_remote_source.dart';
 import 'package:exam/modules/person/data/repository/person_repository.dart';
 
